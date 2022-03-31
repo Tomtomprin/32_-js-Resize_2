fc8ab85bf5bd254848bf54a3dc8a0ba9aac6b032
Tomtomprin <97924842+Tomtomprin@users.noreply.github.com>
2022-03-31 15:00:02 +0900

    Fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(backColorStyle);
 
 window.addEventListener('resize', () => {
     let windowWidth = window.innerWidth;
-    if (windowWidth > 1000) {
+    if (windowWidth <= 1000) {
         // ↓もし、黄色か水色であったら、backgroundColorで背景色をピンクに変え、また、js上のbackColorStyleの値もピンクに上書きする。
         if (backColorStyle === 'rgb(255, 255, 0)' || backColorStyle === 'rgb(224, 255, 252)') {
             console.log('script11行目の記述のconsole');
@@ -14,7 +14,7 @@ window.addEventListener('resize', () => {
             backColorStyle = 'rgb(255, 224, 224)';
         }
     }
-    if (windowWidth <= 1000) {
+    if (windowWidth > 1000) {
         // ↓もし、黄色かピンクであったら、backgroundColorで背景色を水色に変え、また、js上のbackColorStyleの値も水色に上書きする。
 
         if (backColorStyle === 'rgb(255, 224, 224)' || backColorStyle === 'rgb(255, 255, 0)') {
